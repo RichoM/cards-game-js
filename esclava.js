9d3812035a2720424b4dc7771aba86c2477b150a
Ricardo Moran <richi.moran@gmail.com>
2020-03-28 13:05:42 -0300

    Added buttons for joining an existing game.

--- esclava.js
+++ esclava.js
@@ -1,11 +1,24 @@
+let userName = null;
+let currentGame = null;
+
 function displayState(state) {
   if (state == "pending") return "Esperando jugadores...";
   else if (state == "playing") return "Jugando";
   else return "Terminado";
 }
 
+function joinGame(gameId) {
+  $("#lobby").remove();
+  currentGame = db.collection("games").doc(gameId);
+  currentGame.update({
+    playerNames: firebase.firestore.FieldValue.arrayUnion(userName)
+  });
+  $("#game-id").text(gameId);
+  $("#game").show();
+}
+
 $(document).ready(function () {
-  let userName = prompt("Nombre de usuario?");
+  userName = prompt("Nombre de usuario?");
 
   db.collection("games").onSnapshot(snapshot => {
     let $tbody = $("#games-table tbody");
@@ -18,17 +31,19 @@ $(document).ready(function () {
         .append($("<td>").text(game.playerNames.join(",")));
       let $btn = $("<button>")
         .addClass("btn").addClass("btn-sm").addClass("btn-secondary")
-        .text("Unirse");
+        .text("Unirse")
+        .on("click", () => joinGame(doc.id));
       $row.append($("<td>").append($btn));
       $tbody.append($row);
     });
   });
 
   $("#new-game-button").on("click", function () {
+    $("#lobby").remove();
     db.collection("games").add({
       timestamp: new Date(),
       state: "pending",
-      playerNames: [userName]
-    });
+      playerNames: []
+    }).then(doc => joinGame(doc.id));
   });
 });
